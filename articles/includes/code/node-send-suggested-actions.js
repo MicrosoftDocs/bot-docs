f377688d729f630b6b796d3a4e5dfab6cec3412d
Kim Brandl <kbrandl@users.noreply.github.com>
2017-05-19 10:03:54 -0700

    Fixed bug in code sample. (#112)

--- articles/includes/code/node-send-suggested-actions.js
+++ articles/includes/code/node-send-suggested-actions.js
@@ -1,11 +1,14 @@
 // <sendSuggestedActions>
 var msg = new builder.Message(session)
-            .text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?")
-            .suggestedActions([
-                builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
-                builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
-                builder.CardAction.imBack(session, "productId=1&color=red", "Red")
-            ]);
-        session.send(msg);
+	.text("Thank you for expressing interest in our premium golf shirt! What color of shirt would you like?")
+	.suggestedActions(
+		builder.SuggestedActions.create(
+				session, [
+					builder.CardAction.imBack(session, "productId=1&color=green", "Green"),
+					builder.CardAction.imBack(session, "productId=1&color=blue", "Blue"),
+					builder.CardAction.imBack(session, "productId=1&color=red", "Red")
+				]
+			));
+session.send(msg);
 // </sendSuggestedActions>
 
