9d6d16ea47d790e60b2b4c5fef799dea56ba2518
mHulb <jan.niklas.bluhm@gmail.com>
2017-05-31 12:32:09 +0200

    expand validationFunction
    Hello everyone!
    After working through this chapter of the documentation I propose to expand the code-snippet of the validation function for the SandwichOrder's FormBuilder.
    In the style of the other examples, there should be more surrounding code to make it easier for inexperienced readers to understand where the validation function should be added to. During my first read-through, I had to pull up and search the corresponding sample project to understand where this code should live, which was different than other parts of the documentation previously.

--- articles/includes/code/dotnet-formflow-advanced.cs
+++ articles/includes/code/dotnet-formflow-advanced.cs
@@ -1,17 +1,24 @@
 // <validationFunction>
-.Field(nameof(Toppings),
-    validate: async (state, value) =>
-    {
-        var values = ((List<object>)value).OfType<ToppingOptions>();
-        var result = new ValidateResult { IsValid = true, Value = values };
-        if (values != null && values.Contains(ToppingOptions.Everything))
-        {
-            result.Value = (from ToppingOptions topping in Enum.GetValues(typeof(ToppingOptions))
-                            where topping != ToppingOptions.Everything && !values.Contains(topping)
-                            select topping).ToList();
-        }
-        return result;
-    })
+public static IForm<SandwichOrder> BuildForm()
+{
+    ...
+    return new FormBuilder<SandwichOrder>()
+        .Message("Welcome to the sandwich order bot!")
+        ...
+        .Field(nameof(Toppings),
+            validate: async (state, value) =>
+            {
+                var values = ((List<object>)value).OfType<ToppingOptions>();
+                var result = new ValidateResult { IsValid = true, Value = values };
+                if (values != null && values.Contains(ToppingOptions.Everything))
+                {
+                    result.Value = (from ToppingOptions topping in Enum.GetValues(typeof(ToppingOptions))
+                                    where topping != ToppingOptions.Everything && !values.Contains(topping)
+                                    select topping).ToList();
+                }
+                return result;
+        })
+        .Message("For sandwich toppings you have selected {Toppings}.")
 .Message("For sandwich toppings you have selected {Toppings}.")
 // </validationFunction>
 
