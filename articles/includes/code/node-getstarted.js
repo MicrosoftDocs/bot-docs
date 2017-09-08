3aa4aa88ce214ca5c4ee91c27419567fc65f7694
Michael Szul <michael@szul.us>
2017-09-08 11:10:52 -0400

    Added ConsoleConnector language and example (#44)
    * Update bot-builder-nodejs-send-suggested-actions.md
    
    * Added ConsoleConnector language and example

--- articles/includes/code/node-getstarted.js
+++ articles/includes/code/node-getstarted.js
@@ -1,4 +1,15 @@
+// <consolebot>
+
+var builder = require('botbuilder');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
+// </consolebot>
+
 // <echobot>
+
 var restify = require('restify');
 var builder = require('botbuilder');
 
